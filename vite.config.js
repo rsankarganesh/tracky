e80c8d3117777d8aa14113f63be9f4c6a74a29ec
CodeExplorer <107112799+rsankarganesh@users.noreply.github.com>
2025-11-27 08:53:36 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: This tells the app it is hosted at https://rsankarganesh.github.io/tracky/
   base: "/tracky/", 
 })
